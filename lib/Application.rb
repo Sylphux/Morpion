4ba3d5635a07391d236d4039ed90f8bf6db18051
Miranda <lucasmiranda.pro@gmail.com>
2025-09-25 11:18:21 +0200

    test

--- lib/Application.rb
+++ lib/Application.rb
@@ -2,7 +2,7 @@ class Application
   def perform
     # TO DO : méthode qui initialise le jeu puis contient des boucles while pour faire tourner le jeu tant que la partie n'est pas terminée.
     # 
-
+    puts "j'aime les frites"
   end
 
 end
