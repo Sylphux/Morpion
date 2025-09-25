222f672d80109732ad1fd83ce7637c2256fef1e8
Miranda <lucasmiranda.pro@gmail.com>
2025-09-25 13:25:38 +0200

    bug pull

--- lib/BoardCase.rb
+++ lib/BoardCase.rb
@@ -1,14 +1,25 @@
 class BoardCase
 #TO DO : la classe a 2 attr_accessor, sa valeur en string (X, O, ou vide), ainsi que son identifiant de case
-attr_accessor :value, :valuecase
+attr_accessor :value, :valuecase, :all_case
+
+@@all_case
 
   def initialize(value, valuecase)
     #TO DO : doit régler sa valeur, ainsi que son numéro de case
     @value = value
     @valuecase = valuecase
+    @all_case << self
   end
 
-  #rien à rajouter dans cette classe.
-  #Les valuecases seront modifiées par d'autres classes
+  def self.all 
+    @@all_case
+  end
 
+  def self.case(location)
+    @fill_board
+  end
+
+  def 
+    
+  end
 end
--- lib/Show.rb
+++ lib/Show.rb
@@ -2,11 +2,7 @@ class Show
 
   def show_board(board)
     #TO DO : affiche sur le terminal l'objet de classe Board en entrée. S'active avec un Show.new.show_board(instance_de_Board)
-    Board.return_ba
-
-    while 
-      
-    end
+    
   end
 
 end
